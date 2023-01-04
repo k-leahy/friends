bde2b77fd15f554c122b0c175cdc290a341ae70f
Kelly <119141863+k-leahy@users.noreply.github.com>
2023-01-04 12:34:49 -0700

    Add ann

--- friends.rb
+++ friends.rb
@@ -1,7 +1,12 @@
-best_friend_name = "Sammy"
+best_friend_name = "Sam"
+good_friend = "Ann"
 
 if best_friend_name == "Sam"
     p "You're the best!"
-elsif best_friend_name != "Sam"
-    p "Nice to meet you!"
+elsif good_friend = "Ann"
+    p "You're a good friend!"
+elsif best_friend_name != "Sam" or good_friend != "Ann"
+    p "Nice to see you!"
 end
+
+
