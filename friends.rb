356c8914fb633622511668d4dd43bd35dfe861c1
Kelly <119141863+k-leahy@users.noreply.github.com>
2023-01-04 12:35:55 -0700

    Fix1

--- friends.rb
+++ friends.rb
@@ -3,7 +3,7 @@ good_friend = "Ann"
 
 if best_friend_name == "Sam"
     p "You're the best!"
-elsif good_friend = "Ann"
+elsif good_friend == "Ann"
     p "You're a good friend!"
 elsif best_friend_name != "Sam" or good_friend != "Ann"
     p "Nice to see you!"
